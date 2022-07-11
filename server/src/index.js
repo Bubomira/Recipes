6170898e1dde63a621db16a6da546ba53be7af11
Bubomira <106514492+Bubomira@users.noreply.github.com>
2022-07-12 00:57:56 +0300

    set up db and router file

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
+
+const start = async()=>{
+    try{
+        await mongoose.connect('mongodb://localhost:27017/recipes')
+        console.log('Connected to database!')
+    }catch(err){
+        console.log(err)
+    }
+}
+start();
 
 app.listen('3030',()=>console.log('App is listening on port 3030...'))
