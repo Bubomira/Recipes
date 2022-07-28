359450debe5e656f205022bc13f1d78421bd620b
Bubomira <106514492+Bubomira@users.noreply.github.com>
2022-07-28 12:37:41 +0300

    added imageUrl to recipe model

--- server/src/models/Recipe.js
+++ server/src/models/Recipe.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({
     title:{type:String,required:true},
     ingridients:{type:String,required:true},
-    steps:{type:String,required:true}
+    steps:{type:String,required:true},
+    imageUrl:{type:String,required:true},
     type:{type:String,required:true,enum:['dessert','dish']},
     ownerId:{type:mongoose.Types.ObjectId,ref:'User'},
     likes:{type:Number},
