255fb3a0cd1cf3d411767785a8db7cf5051f52b6
Bubomira <106514492+Bubomira@users.noreply.github.com>
2022-08-03 01:19:58 +0300

    updated home component

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,37 +1,37 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-import  RecipeHomeCard from '../recipe-components/recipe-home-card/RecipeHomeCard'
+import RecipeHomeCard from '../recipe-components/recipe-home-card/RecipeHomeCard'
 
-import {getLatestRecipes} from '../../services/recipeService'
+import { getLatestRecipes } from '../../services/recipeService'
 
 import './Home.css'
 
-export default function Home(){
-    const [recipes,setRecipes] = useState([])
-    useEffect(()=>{
-        getLatestRecipes().then(recipes=>{
-         setRecipes(recipes)
+export default function Home() {
+    const [recipes, setRecipes] = useState([])
+    useEffect(() => {
+        getLatestRecipes().then(recipes => {
+            setRecipes(recipes)
         })
-    },[])
-    return(
+    }, [])
+    return (
         <div className="home-wrapper">
-        <div className="greeter">
-           If you want to see many tasty recipes, u are at the right place!
-        </div>
-        <section className="first-three-recipes">
-            {recipes.length==0 ? 
-            <h3 className="home-no-recipes">Recipes are yet to be added</h3>
-            :
-            recipes.map(x=><RecipeHomeCard key={x._id} recipe={x}/>)
-            }
-        </section>
-          <p className="get-started">
-            Lets get started: 
-            <Link to='/login'>  Sign in </Link>
-             or  
-             <Link to='/register'>  Sign up </Link>
-            </p> 
+            <div className="greeter">
+                If you want to see many tasty recipes, u are at the right place!
+            </div>
+            <section className="first-three-recipes">
+                {recipes.length == 0 ?
+                    <h3 className="home-no-recipes">Recipes are yet to be added</h3>
+                    :
+                    recipes.map(x => <RecipeHomeCard key={x._id} recipe={x} />)
+                }
+            </section>
+            <p className="get-started">
+                Lets get started:
+                <Link to='/login'>  Sign in </Link>
+                or
+                <Link to='/register'>  Sign up </Link>
+            </p>
         </div>
     )
 }
