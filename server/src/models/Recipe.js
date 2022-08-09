6274d702bfb5d9871aac1129600633bad65ca199
Bubomira <106514492+Bubomira@users.noreply.github.com>
2022-08-10 01:28:09 +0300

    recipe and user schemas with validation

--- server/src/models/Recipe.js
+++ server/src/models/Recipe.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
-    title:{type:String,required:true},
-    ingridients:{type:String,required:true},
-    steps:{type:String,required:true},
-    imageUrl:{type:String,required:true},
+    title:{type:String,required:[true,'Title is required!'],minLength:[3,'Title should be at least 3 characters']},
+    ingridients:{type:String,required:[true,'Ingridients are required!'],minLength:[10,'Ingridients should be at least 3 characters']},
+    steps:{type:String,required:[true,'Steps are required!'],minLength:[15,'Steps should be at least 3 characters']},
+    imageUrl:{type:String,
+        required:[true,'Image url is required'],
+        validate:{
+            validator:(v)=>{
+                return v.startsWith('https://')
+            },
+            message:'Image url should start with https://'
+        }
+},
     type:{type:String,required:true,enum:['dessert','dish']},
     ownerId:{type:mongoose.Types.ObjectId,ref:'User'},
     likes:{type:Number},
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     username:{type:String,required:[true,'Username is required!'],minLength:[3,'Username should be at least 3 characters!']},
     email:{type:String,
         required:[true,'Email is required!'],
-        minLength:[5,'Email should be at least 3 characters!'],
+        minLength:[5,'Email should be at least 5 characters!'],
         validate:{
             validator:(v)=>{
                 return v.includes('@')
