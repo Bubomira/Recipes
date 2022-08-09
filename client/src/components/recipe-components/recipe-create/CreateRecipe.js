be7cbd2352158d9dd99e4442125e0d93769f863e
Bubomira <106514492+Bubomira@users.noreply.github.com>
2022-08-10 00:47:19 +0300

    create recipe validation added

--- client/src/components/recipe-components/recipe-create/CreateRecipe.js
+++ client/src/components/recipe-components/recipe-create/CreateRecipe.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { createRecipe } from '../../../services/recipeService';
 
@@ -7,99 +7,140 @@ import './CreateRecipe.css'
 
 export default function CreateRecipe() {
     const navigate = useNavigate()
-  let [values, setValues] = useState({
-    title: '',
-    ingridients: '',
-    imageUrl: '',
-    steps: '',
-    type: '',
-  })
-  const onChangeHandler = (e) => {
-    setValues(oldState => ({
-      ...oldState,
-      [e.target.name]: e.target.value
-    }))
-  }
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    createRecipe(values).then(()=>{
-        navigate('/recipeCatalog')
+    let [errors, setErrors] = useState({
+        title: '',
+        ingridients: '',
+        imageUrl: '',
+        steps: ''
     })
-    
-  }
-  return (
-    <div className='wrapper'>
-      <div className="create-form">
-        <form onSubmit={onSubmitHandler}>
-          <h1 >Create recipe</h1 >
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control item"
-              placeholder="Title"
-              name="title"
-              value={values.title}
-              onChange={onChangeHandler}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control item"
-              placeholder="Ingridients"
-              name="ingridients"
-              value={values.ingridients}
-              onChange={onChangeHandler}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control item"
-              placeholder="ImageUrl"
-              name="imageUrl"
-              value={values.imageUrl}
-              onChange={onChangeHandler}
-            />
-          </div>
-          <div className="form-group">
-            <textarea
-              className="form-control item"
-              placeholder="Steps to prepare"
-              name="steps"
-              value={values.steps}
-              onChange={onChangeHandler}
-            />
-          </div>
-          <div >
-            <label htmlFor='dish'> Dish: </label>
-            <input
-              type="radio"
-              name="type"
-              id='dish'
-              className="radioElement"
-              value="dish"
-              checked={values.type=='dish'}
-              onChange={onChangeHandler}
-            />
-            <label htmlFor='dish'> Dessert:</label>
-            <input
-              type="radio"
-              name="type"
-              id="dessert"
-              className="radioElement"
-              value="dessert"
-              checked={values.type=='dessert'}
-              onChange={onChangeHandler}
-            />
-          </div>
-          <div className="form-group">
-            <button type="submit" className="create-recipe">
-              Create Recipe
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  )
+    let [values, setValues] = useState({
+        title: '',
+        ingridients: '',
+        imageUrl: '',
+        steps: '',
+        type: '',
+    })
+    const onChangeHandler = (e) => {
+        setValues(oldState => ({
+            ...oldState,
+            [e.target.name]: e.target.value
+        }))
+    }
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        createRecipe(values).then(() => {
+            navigate('/recipeCatalog')
+        }).catch(()=>{
+            navigate('/404')
+        })
+    }
+
+    const lengthValidator = (e, minLength) => {
+        setErrors(oldErrors => ({
+            ...oldErrors,
+            [e.target.name]: values[e.target.name].length < minLength
+        }))
+    }
+    const imageUrlValidator = () => {
+        setErrors(oldErrors => ({
+            ...oldErrors,
+            imageUrl: !values.imageUrl.startsWith('https://')
+        }))
+    }
+
+    return (
+        <div className='wrapper'>
+            <div className="create-form">
+                <form onSubmit={onSubmitHandler}>
+                    <h1 >Create recipe</h1 >
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control item"
+                            placeholder="Title"
+                            name="title"
+                            value={values.title}
+                            onChange={onChangeHandler}
+                            onBlur={(e) => lengthValidator(e, 3)}
+                        />
+                        {errors.title &&
+                            <p className='create-recipe-error'>Title should be at least 3 characters</p>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control item"
+                            placeholder="Ingridients"
+                            name="ingridients"
+                            value={values.ingridients}
+                            onChange={onChangeHandler}
+                            onBlur={(e) => lengthValidator(e, 10)}
+                        />
+                        {errors.ingridients &&
+                            <p className='create-recipe-error'>ingridients should be at least 10 characters </p>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control item"
+                            placeholder="ImageUrl"
+                            name="imageUrl"
+                            value={values.imageUrl}
+                            onChange={onChangeHandler}
+                            onBlur={imageUrlValidator}
+                        />
+                        {errors.imageUrl &&
+                            <p className='create-recipe-error'>Image url should start with https://</p>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <textarea
+                            className="form-control item"
+                            placeholder="Steps to prepare"
+                            name="steps"
+                            value={values.steps}
+                            onChange={onChangeHandler}
+                            onBlur={(e) => lengthValidator(e, 15)}
+                        />
+                        {errors.steps &&
+                            <p className='create-recipe-error'>Steps to prepare should be at least 15 characters </p>
+                        }
+                    </div>
+                    <div >
+                        <label htmlFor='dish'> Dish: </label>
+                        <input
+                            type="radio"
+                            name="type"
+                            id='dish'
+                            className="radioElement"
+                            value="dish"
+                            checked={values.type == 'dish'}
+                            onChange={onChangeHandler}
+                        />
+                        <label htmlFor='dish'> Dessert:</label>
+                        <input
+                            type="radio"
+                            name="type"
+                            id="dessert"
+                            className="radioElement"
+                            value="dessert"
+                            checked={values.type == 'dessert'}
+                            onChange={onChangeHandler}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button
+                            type="submit"
+                            className="create-recipe"
+                            disabled={Object.values(errors).some(x => x == true) || Object.values(values).some(x => x == '')}
+                        >
+                            Create Recipe
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
